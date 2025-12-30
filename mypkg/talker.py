487a3dcf21b21e6fbab96d40211028b9f98d70dd
Kei Take <S24C1076MR@s.chibakoudai.jp>
2025-12-30 20:11:37 +0900

    Revert "サービスの実装"
    This reverts commit 7539781a8b495829e60f7a129c870267d34c6b68.

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,19 +1,22 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.srv import Query
+from person_msgs.msg import Person
 
 rclpy.init()
 node = Node("talker")
+pub = node.create_publisher(Person, "person", 10)
+n = 0
 
 
-def cb(request, response):
-    if request.name == "武田啓太郎":
-        response.age = 19
-    else:
-        response.age = 255
-    
-    return response
+def cb():
+    global n
+    msg = Person()
+    msg.name = "武田啓太郎"
+    msg.age = n
+    pub.publish(msg)
+    n += 1
+
 
 def main():
-    srv = node.create_service(Query,"query", cb)
+    node.create_timer(0.5, cb)
     rclpy.spin(node)
