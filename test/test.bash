d0e097fd20264fb394ce60bc86fb8d748051fecb
Kei Take <S24C1076MR@s.chibakoudai.jp>
2025-12-31 17:42:01 +0900

    test追加

---
+++ test/test.bash
@@ -0,0 +1,37 @@
+#!/usr/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Keitaro Takeda
+# SPDX-License-Identifier: BSD-3-Clause
+
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+ng() {
+    echo "${1}行目が違うよ"
+    res=1
+}
+res=0
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+
+timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+cat /tmp/mypkg.log |
+
+if grep '以上';then
+  echo Ok
+else
+  ng "$LINENO"
+fi
+
+
+cat /tmp/mypkg.log |
+
+if grep '回目';then
+  echo Ok
+else
+  ng "$LINENO"
+fi
+
+[ "$res" = 0 ] && echo OK
+exit $res
